0931bc0c3349a3d928e15a7cc6d11202badd7384
ByungJoon Lee <imjuni@gmail.com>
2023-04-17 10:12:23 +0900

    Enhance getErrorCode
    * enhance getErrorCode: add new parameter defaultCode, encryptor
    * version change: 0.6.0 > 0.6.1

--- src/modules/getErrorCode.ts
+++ src/modules/getErrorCode.ts
@@ -1,7 +1,17 @@
-export default function getErrorCode(err: Error) {
+import type { TMaeumEncryptor } from '#modules/interfaces/MaeumFunctions';
+
+export default function getErrorCode(
+  err: Error,
+  defaultCode?: string,
+  encryptor?: TMaeumEncryptor,
+) {
   try {
     if ('code' in err && typeof err.code === 'string') {
-      return err.code;
+      return encryptor != null ? encryptor(err.code) : err.code;
+    }
+
+    if (defaultCode != null) {
+      return encryptor != null ? encryptor(defaultCode) : defaultCode;
     }
 
     return undefined;
