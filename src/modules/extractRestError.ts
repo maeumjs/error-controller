d02973bc45de153dbb175341973a2e81f0b5e3b8
ByungJoon Lee <imjuni@gmail.com>
2023-04-27 14:32:18 +0900

    Add new function `extractRestError`
    * add new function extractRestError: extractRestError function extract IMaeumRestError interface data from unknown data
    * version change: 0.8.1 > 0.8.2

---
+++ src/modules/extractRestError.ts
@@ -0,0 +1,27 @@
+import type IMaeumRestError from './interfaces/IMaeumRestError';
+
+function isValidType(value: unknown): value is string | number {
+  return typeof value === 'string' || typeof value === 'number';
+}
+
+export default function extractRestError<T = unknown>(
+  unknownError: T,
+): IMaeumRestError | undefined {
+  try {
+    const message: unknown =
+      unknownError != null && typeof unknownError === 'object' && 'message' in unknownError
+        ? unknownError.message
+        : undefined;
+    const code: unknown =
+      unknownError != null && typeof unknownError === 'object' && 'code' in unknownError
+        ? unknownError.code
+        : undefined;
+
+    if (isValidType(message) && isValidType(code)) {
+      return { message: `${message}`, code: `${code}` };
+    }
+    return undefined;
+  } catch {
+    return undefined;
+  }
+}
